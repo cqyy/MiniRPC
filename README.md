4a8002873ed6abb79a848e4be7c10e9fb565f844
cqyy <527068423@qq.com>
2014-08-05 17:09:06 +0800

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+##About Mini RPC
+
+微缩版的RPC服务器，参考Hadoop中RPC服务器的架构。
--- src/main/java/com/cqyuanye/ipc/Server.java
+++ src/main/java/com/cqyuanye/ipc/Server.java
@@ -1,11 +1,47 @@
 package com.cqyuanye.ipc;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
 /**
  * Created by Kali on 14-8-4.
  */
 class Server {
 
-    
+    private class Connection{
+        private final SocketChannel channel;
+        private final LinkedList<Call> calls = new LinkedList<>();
+
+        public Connection(SocketChannel channel){
+            this.channel = channel;
+        }
+
+        public void send(Call call){
+            if (!calls.isEmpty()){
+                calls.addLast(call);
+            }
+            call = calls.getFirst();
+            try {
+                channel.write(call.response);
+                if (call.response.hasRemaining()){
+                    calls.addFirst(call);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private class Call{
+        private int index;
+        private Class[] paramClass;
+        private Object[] params;
+        private ByteBuffer response;
+    }
 
     private class Listener extends Thread{}
 
